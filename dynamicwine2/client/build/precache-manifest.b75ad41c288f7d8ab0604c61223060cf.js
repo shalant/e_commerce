e2dd4c2b7b1a58ea6fc22d761af3bdfc35a9179f
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-03-13 20:59:05 -0600

    finished video 1

---
+++ dynamicwine2/client/build/precache-manifest.b75ad41c288f7d8ab0604c61223060cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3dcb17af376da09853c941cebe17f82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e4054ffd826a2d5b855",
+    "url": "/static/css/2.c72968ad.chunk.css"
+  },
+  {
+    "revision": "7341a64b2b602fe7d34d",
+    "url": "/static/css/main.b51bd8b5.chunk.css"
+  },
+  {
+    "revision": "5e4054ffd826a2d5b855",
+    "url": "/static/js/2.3e64fd25.chunk.js"
+  },
+  {
+    "revision": "5a189a6ad4a9d736fbeccf60cc560855",
+    "url": "/static/js/2.3e64fd25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7341a64b2b602fe7d34d",
+    "url": "/static/js/main.0277cda4.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
