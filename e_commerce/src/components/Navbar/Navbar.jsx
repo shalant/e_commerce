78afc169160bbe3b2a4667da45f206a850d6d2eb
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-09-16 22:18:09 -0500

    got up to 41.47

--- e_commerce/src/components/Navbar/Navbar.jsx
+++ e_commerce/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
-import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography, Icon } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
 
+import logo from '../../assets/commerce.png';
+import useStyles from './styles';
+
 const Navbar = () => {
+    const classes = useStyles();
+
     return (
         <>
             <AppBar position='fixed' className={classes.appBar} color='inherit'>
                 <Toolbar>
                     <Typography variant='h6' className>
-                        <img src={} alt='Commerce.js' height='25px' className={classes.image} />
+                        <img src={logo} alt='Commerce.js' height='25px' className={classes.image} />
                         Commerce.js
                     </Typography>
+                    <div className={classes.grow} />
+                    <div className={classes.button}>
+                        <IconButton aria-label="Show cart items" color='inherit' >
+                            <Badge badgeContent={2} color='secondary'>
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                    </div>
                 </Toolbar>
             </AppBar>  
         </>
--- e_commerce/src/components/Products/Product/Product.jsx
+++ e_commerce/src/components/Products/Product/Product.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
+import comPic from '../../../assets/commerce.png';
 
 import useStyles from './styles';
 
 const Product = ({ product }) => {
     const classes = useStyles();
-    
+
     return (
         <Card className={classes.root} >
-            <CardMedia className={classes.media} image={product.image} title={product.name} />
+            <CardMedia className={classes.media} image={comPic} title={product.name} />
             <CardContent>
                 <div className={classes.cardContent}>
                     <Typography variant='h5' gutterBottom>
@@ -27,7 +28,7 @@ const Product = ({ product }) => {
                 </IconButton>
             </CardActions>
         </Card>
-    )
-}
+    );
+};
 
 export default Product;
