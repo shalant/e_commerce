d42441499cc2b52cad02fb6afa4d27e01e66ac52
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-09-20 15:31:07 -0500

    added react router

--- e_commerce/src/components/Cart/Cart.jsx
+++ e_commerce/src/components/Cart/Cart.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Container, Typography, Button, Grid } from '@material-ui/core';
 
 import useStyles from './styles';
+import CartItem from './CartItem/CartItem';
 
 const Cart = ({ cart }) => {
-    const isEmpty = !cart.line_items.length;
     const classes = useStyles();
 
     const EmptyCart = () => (
@@ -16,8 +16,7 @@ const Cart = ({ cart }) => {
             <Grid container spacing={3}>
                 {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id}>
-                        {/* <CartItem /> */}
-                        <div>{item.name}</div>
+                        <CartItem item={item} />
                     </Grid>
                 ))}
             </Grid>
@@ -29,13 +28,15 @@ const Cart = ({ cart }) => {
                     </div>
             </div>
         </>
-    )
+    );
+
+    if(!cart.line_items) return 'Loading...';
 
     return (
         <Container>
             <div className={classes.toolbar} />
-            <Typography className={classes.title} variant='h3'>Your Shopping Cart</Typography>
-            { isEmpty ? <EmptyCart /> : <FilledCart />}
+            <Typography className={classes.title} variant='h3' gutterBottom>Your Shopping Cart</Typography>
+            { !cart.line_items.length ? <EmptyCart /> : <FilledCart />}
         </Container>
     )
 }
