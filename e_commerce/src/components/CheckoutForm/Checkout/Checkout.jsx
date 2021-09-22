d66df9e267b5dcb344661827487183aa84d4ff56
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-09-22 10:55:39 -0500

    working on tokens and the form

--- e_commerce/src/components/CheckoutForm/Checkout/Checkout.jsx
+++ e_commerce/src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,15 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
 
+import { commerce } from '../../../lib/commerce';
 import useStyles from './styles';
 import AddressForm from '../AddressForm';
 import PaymentForm from '../PaymentForm';
 
 const steps = ['Shipping address', 'Payment details'];
 
-const Checkout = () => {
+const Checkout = ({ cart }) => {
     const [activeStep, setActiveStep] = useState(0);
+    const [checkoutToken, setCheckoutToken] = useState(null)
     const classes = useStyles();
+
+    useEffect(() => {
+        const generateToken = async () => {
+            try {
+                const token = await commerce.checkout.generateToken(cart.id, { type: 'cart' })
+
+                console.log(token);
+
+                setCheckoutToken(token);
+            } catch (error) {
+                
+            }
+        }
+
+        generateToken();
+    }, []);
 
     const Confirmation = () => (
         <div>
@@ -27,7 +45,7 @@ const Checkout = () => {
             <main className={classes.layout}>
                 <Paper className={classes.paper}>
                     <Typography variant='h4' align='center'>Checkout</Typography>
-                    <Stepper activeStep={0} className={classes.stepper}>
+                    <Stepper activeStep={activeStep} className={classes.stepper}>
                         {steps.map((step) => (
                             <Step key={step}>
                                 <StepLabel>{step}</StepLabel>
