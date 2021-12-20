e9de149d4f980fa2b05c096ad86fff856a9279f0
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-12-20 15:50:52 -0600

    finished

--- react-weather/src/App.js
+++ react-weather/src/App.js
@@ -34,7 +34,12 @@ function App() {
 
 
   return (
-    <div className={(typeof weather.main !='undefined')}>
+    <div className={
+      (typeof weather.main !='undefined') 
+        ? ((weather.main.temp > 16) 
+          ? 'app warm' 
+          : 'app') 
+        : 'app'}>
       <main>
         <div className='search-box'>
           <input
